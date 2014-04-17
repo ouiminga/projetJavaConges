21d911c8d4cbce316cfeef9684728e1b450d8a5c
ouiminga <ouimaz@yahoo.fr>
2014-04-17 13:28:34 +0200

    jusqua class utilitaire

---
+++ src/Utilitaires.java
@@ -0,0 +1,9 @@
+
+public abstract class Utilitaires extends Vehicule  {
+
+	public Utilitaires(){
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
---
+++ src/Voitures.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ouiminga
+ *
+ */
+public abstract class Voitures extends Vehicule{
+
+	/**
+	 * 
+	 */
+	public Voitures() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
